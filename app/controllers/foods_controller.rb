f5d7e59a499a94053adf68a0db601348fb6a618d
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-19 10:38:13 -0600

    Update foods controller with FoodInfo

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   def index
-    @foods_info = SearchFacade.find_foods_info(params[:q])
+    @foods_total = SearchFacade.find_foods_info(params[:q])
+    @foods_info = FoodInfo.all.limit(10)
   end
 end
