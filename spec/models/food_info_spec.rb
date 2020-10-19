d9aa6365f29cda94f00d7de082e0e69613bbf082
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-19 10:16:13 -0600

    Add food info model spec

---
+++ spec/models/food_info_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe FoodInfo do
+  describe 'Validations' do
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :gtin_code}
+    it {should validate_presence_of :owner}
+    it {should validate_presence_of :ingredients}
+  end
+end
