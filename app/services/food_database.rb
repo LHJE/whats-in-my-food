f95ed24c1770f0477866d1f8de4045d658f2fe6b
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-19 09:35:16 -0600

    Add food database to make API call to collect food data

---
+++ app/services/food_database.rb
@@ -0,0 +1,16 @@
+class FoodDatabase
+  def self.food_details(food)
+    key = ENV['FOOD_DATA_API_KEY']
+    url = 'https://api.nal.usda.gov'
+    uri = "/fdc/v1/foods/search?api_key=#{key}&query=#{food}"
+    get_results(url, key, uri)
+  end
+
+  def self.get_results(url, key, uri)
+    conn = Faraday.new(url: url) do |faraday|
+      faraday.headers['X-API-Key'] = key
+    end
+    results = conn.get(uri)
+    JSON.parse(results.body, symbolize_names: true)
+  end
+end
