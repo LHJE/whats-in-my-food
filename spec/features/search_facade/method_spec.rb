4cbffc4a0405c456ae930854c2df326ae6e22fd5
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-19 10:29:43 -0600

    Add spec for search facade so FoodInfo can be made there

---
+++ spec/features/search_facade/method_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Search Facade' do
+  describe 'As a visitor' do
+    before :each do
+      visit root_path
+      fill_in :q, with: 'sweet potatoes'
+      click_button "Search"
+    end
+
+    it "can create rows on a FoodInfo table" do
+      expect(FoodInfo.all.count).to eq(10)
+    end
+  end
+end
