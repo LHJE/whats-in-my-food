f102054988497ec91b7418f309d1a59b8eced546
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-19 09:20:31 -0600

    Add routing test to index spec

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -15,5 +15,12 @@ RSpec.describe 'Welcome Index' do
 
       click_button "Search"
     end
+
+    it "can send me to '/foods' when I click Search button" do
+      fill_in :q, with: 'sweet potatoes'
+      click_button "Search"
+
+      expect(current_path).to eq("/foods")
+    end
   end
 end
