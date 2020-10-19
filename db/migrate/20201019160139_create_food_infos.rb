c329244cbe9fb424850847f644bad6b818b01fec
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-19 10:37:02 -0600

    Update food_infos migration d/t error: ... 'ActiveModel::RangeError: 70560951975 is out of range for ActiveModel::Type::Integer with limit 4 bytes'

--- db/migrate/20201019160139_create_food_infos.rb
+++ db/migrate/20201019160139_create_food_infos.rb
@@ -2,7 +2,7 @@ class CreateFoodInfos < ActiveRecord::Migration[5.2]
   def change
     create_table :food_infos do |t|
       t.string :description
-      t.integer :gtin_code
+      t.string :gtin_code
       t.string :owner
       t.string :ingredients
 
